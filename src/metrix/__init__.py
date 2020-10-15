6223bcc047413fea94e0598b15dab2493ec9b6b2
Burton DeWilde <burtdewilde@gmail.com>
2020-10-15 13:08:04 -0500

    Declare version in separate module
    trying to get readthedocs to build the pkg without an error

--- src/metrix/__init__.py
+++ src/metrix/__init__.py
@@ -1,6 +1,10 @@
+from metrix.about import __version__
+# TODO: declare __version__ here rather than importing about.__version__
+# once readthedocs can use sufficiently new version of setuptools
+# i'm pretty sure that's the reason builds keep failing :/
+# __version__ = "0.1.0"
+
 from metrix.element import MElement
 from metrix.sinks import *
 from metrix.stream import MStream
 from metrix.coordinator import MCoordinator
-
-__version__ = "0.1.0"
